47dbf94ad0c0bba020df8258944f2c83f8cee815
y060 <060yoyo@gmail.com>
2024-03-29 23:32:48 +0800

    scrollbar & mousemove

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener("mousemove", function(e){
         let x = e.pageX;
         let y = e.pageY;
-        circle.style.left = x-14+"px";
+        circle.style.left = x > (window.innerWidth-70)? window.innerWidth-50+"px": x-14+"px";
         circle.style.top = y-14+"px";
+        console.log("x: ", x, "innerWidth: ", window.innerWidth)
+        console.log( x > (window.innerWidth-70))
     })
 
 })
